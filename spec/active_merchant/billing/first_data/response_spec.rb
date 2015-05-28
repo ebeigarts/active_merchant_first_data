bbd3d6a417f055d100abd34f2bf4d9a08628c4f0
Sacristan <girts.kesteris@gmail.com>
2015-05-28 14:26:27 +0300

    Added response tests

--- spec/active_merchant/billing/first_data/response_spec.rb
+++ spec/active_merchant/billing/first_data/response_spec.rb
@@ -2,4 +2,119 @@
 require "spec_helper"
 
 describe ActiveMerchant::Billing::FirstData::Response do
+
+  before :all do
+    @transaction_id = "e+oClP4em8uBDozaZ4CBBbipEcM="
+    @recc_pmnt_id = "test123"
+    @recc_pmnt_expiry = "0120"
+
+    @result_ok = "OK"
+    @result_created = "CREATED"
+
+    @result_code = "000"
+    @three_dee_secure = "AUTHENTICATED"
+  end
+
+  context "basic" do
+    before :all do
+      @response_hash = ActiveSupport::HashWithIndifferentAccess.new
+
+      @response_hash[:transaction_id] = @transaction_id
+      @response_hash[:result] = @result_ok
+      @response_hash[:result_code] = @result_code
+      @response_hash[:result] = @result_created
+      @response_hash[:recc_pmnt_id] = @recc_pmnt_id
+      @response_hash[:recc_pmnt_expiry] = @recc_pmnt_expiry
+      @response_hash[:'3dsecure'] = @three_dee_secure
+
+      @response = ActiveMerchant::Billing::FirstData::Response.new @response_hash
+    end
+
+    # three_dee_secure
+    # parameters
+
+    it "should return same parameters as passed" do
+      expect(@response.parameters).to eq @response_hash
+    end
+
+    it "should return transaction_id" do
+      expect(@response.transaction_id).to eq @transaction_id
+    end
+
+    it "should return transaction_id" do
+      expect(@response.three_dee_secure).to eq @three_dee_secure
+    end
+
+    it "should return correct result for response" do
+      expect(@response.result_code).to eq @result_code
+    end
+  end
+
+  context "business logic methods" do
+    context "result" do
+      before :all do
+        @ok_response_hash = ActiveSupport::HashWithIndifferentAccess.new
+        @nok_response_hash = ActiveSupport::HashWithIndifferentAccess.new
+
+        @ok_response_hash[:result] = @result_ok
+        @nok_response_hash[:result] = @result_created
+
+        @ok_response = ActiveMerchant::Billing::FirstData::Response.new @ok_response_hash
+        @nok_response = ActiveMerchant::Billing::FirstData::Response.new @nok_response_hash
+      end
+
+      it "should be ok if result is ok" do
+        expect(@ok_response.ok?).to eq true
+      end
+
+      it "other than ok should not be ok" do
+        expect(@nok_response.ok?).to eq false
+      end
+
+      it "should return correct result for ok response" do
+        expect(@ok_response.result).to eq @result_ok
+      end
+
+      it "should return correct result for ok response" do
+        expect(@nok_response.result).to eq @result_created
+      end
+    end
+
+    context "recurring" do
+      before :all do
+        @ok_response_hash = ActiveSupport::HashWithIndifferentAccess.new
+        @nok_response_hash = ActiveSupport::HashWithIndifferentAccess.new
+
+        @ok_response_hash[:recc_pmnt_id] = @recc_pmnt_id
+        @ok_response_hash[:recc_pmnt_expiry] = @recc_pmnt_expiry
+
+        @ok_response = ActiveMerchant::Billing::FirstData::Response.new @ok_response_hash
+        @nok_response = ActiveMerchant::Billing::FirstData::Response.new @nok_response_hash
+      end
+
+      it "should be recurring if recc_pmnt_id and recc_pmnt_expiry present" do
+        expect(@ok_response.recurring?).to eq true
+      end
+
+      it "should return false if there is no recc_pmnt_id and recc_pmnt_expiry" do
+        expect(@nok_response.recurring?).to eq false
+      end
+
+      it "should match value for recc_pmnt_id for recurring response" do
+        expect(@ok_response.recc_pmnt_id).to eq @recc_pmnt_id
+      end
+
+      it "should match value for recc_pmnt_expiry for recurring response" do
+        expect(@ok_response.recc_pmnt_expiry).to eq @recc_pmnt_expiry
+      end
+
+      it "should not return value for recc_pmnt_id for recurring response" do
+        expect(@nok_response.recc_pmnt_id).to eq nil
+      end
+
+      it "should not return value for recc_pmnt_expiry for recurring response" do
+        expect(@nok_response.recc_pmnt_expiry).to eq nil
+      end
+    end
+  end
 end
